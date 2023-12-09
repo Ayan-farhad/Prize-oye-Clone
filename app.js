0195f231b7e43f95b4bd5c71dcb71063c3ebdc8c
Ayan-farhad <khaanayan81@gmail.com>
2023-12-09 17:13:52 +0500

    D

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 const arr = [];
-const sameDiv = document.querySelector('.container');
-const checkOut = document.querySelector('.checkout');
+let data;
+const div = document.querySelector('.render-phones');
+const checkout = document.querySelector('#checkout');
+
 
 axios.get('https://fakestoreapi.com/products')
 .then((res)=>{
     console.log(res.data);
-    data = res.data;
-    for (let i = 0; i < res.data.length; i++){
-        sameDiv.innerHTML += `
+    data = res.data
+    for (let i = 0; i < res.data.length; i++) { 
+        div.innerHTML += `<div class="d-flex p-2">
         <div class="card p-1 mb-3 shadow-lg" style="width: 16rem; height: 29rem">
-            <img width ='' height ='200px' src= ${res.data[i].image} class="card-img-top" alt="Product image">
-        <div class="card-body">
-        <h5 class="card-title titleh5">Title: ${res.data[i].title}</h5>
-        <p class="card-text">Category: ${res.data[i].category}</p>
-        <h5 class="card-title">Price: ${res.data[i].price}</h5>
-        <a onclick="addToCart2(${i})" class="btn btn-primary">Add to Cart</a>
-        </div>
-        </div>
+         <img width ='' height ='200px' src= ${res.data[i].image} class="card-img-top" alt="Product image">
+         <div class="card-body">
+         <h5 class="card-title titleh5">Title: ${res.data[i].title}</h5>
+         <p class="card-text">Category: ${res.data[i].category}</p>
+         <h5 class="card-title">Price: ${res.data[i].price}</h5>
+         <a onclick="addToCart(${i})" class="btn btn-primary">Add to Cart</a>
+         </div>
+         </div>
+         </div>
         `
     }
-})
-.catch((err)=>{
+}).catch((err)=>{
     console.log(err);
 })
 
-let data;
-
-function addToCart2(index){
-    console.log(data[index]);
-    arr.push(data[index])
+function addToCart (index){
+    // console.log(data[index]);
+    arr.push(data[index]);
     console.log(arr);
     Swal.fire({
         title: "Good job!",
-        text: "Product added to cart successfully!",
+        text: "Product added to cart successfully",
         icon: "success"
       });
 }
 
-function Checkout(){
-    window.location = "checkout.html";
-    localStorage.setItem( JSON.stringify(phones));
-}
+
+
+checkout.addEventListener('click' , ()=>{
+    localStorage.setItem('items' , JSON.stringify(arr));
+    window.location = 'checkout.html';
+    // localStorage.removeItem('items')
+})
+
