a074b9dcea12218d62567220d7f3c9df9811ce3a
Ayan-farhad <khaanayan81@gmail.com>
2023-12-12 15:39:11 +0500

    continue

--- app.js
+++ app.js
@@ -1,47 +1,53 @@
-const arr = [];
-let data;
-const div = document.querySelector('.render-phones');
-const checkout = document.querySelector('#checkout');
-
+// const arr = [];
+const sameDiv = document.querySelector('.sameDiv');
+const checkOut = document.querySelector('.checkout');
 
 axios.get('https://fakestoreapi.com/products')
 .then((res)=>{
     console.log(res.data);
-    data = res.data
-    for (let i = 0; i < res.data.length; i++) { 
-        div.innerHTML += `<div class="d-flex p-2">
+    data = res.data;
+    for (let i = 0; i < res.data.length; i++){
+        sameDiv.innerHTML += `
         <div class="card p-1 mb-3 shadow-lg" style="width: 16rem; height: 29rem">
-         <img width ='' height ='200px' src= ${res.data[i].image} class="card-img-top" alt="Product image">
-         <div class="card-body">
-         <h5 class="card-title titleh5">Title: ${res.data[i].title}</h5>
-         <p class="card-text">Category: ${res.data[i].category}</p>
-         <h5 class="card-title">Price: ${res.data[i].price}</h5>
-         <a onclick="addToCart(${i})" class="btn btn-primary">Add to Cart</a>
-         </div>
-         </div>
-         </div>
+            <img width ='' height ='200px' src= ${res.data[i].image} class="card-img-top" alt="Product image">
+        <div class="card-body">
+        <h5 class="card-title titleh5">Title: ${res.data[i].title}</h5>
+        <p class="card-text">Category: ${res.data[i].category}</p>
+        <h5 class="card-title">Price: ${res.data[i].price}</h5>
+        <a onclick="addToCart2(${i})" class="btn btn-primary">Add to Cart</a>
+        </div>
+        </div>
         `
     }
-}).catch((err)=>{
+})
+.catch((err)=>{
     console.log(err);
 })
 
-function addToCart (index){
-    // console.log(data[index]);
-    arr.push(data[index]);
+const dataa = JSON.parse (localStorage.getItem("items"))
+
+let arr ;
+if(Array.isArray(dataa)){
+    arr =[...dataa]
+}
+else {
+    arr = []
+}
+let data;
+
+function addToCart2(index){
+    console.log(data[index]);
+    arr.push(data[index])
     console.log(arr);
     Swal.fire({
         title: "Good job!",
-        text: "Product added to cart successfully",
+        text: "Product added to cart successfully!",
         icon: "success"
       });
 }
 
-
-
-checkout.addEventListener('click' , ()=>{
+checkOut.addEventListener('click' , ()=>{
     localStorage.setItem('items' , JSON.stringify(arr));
     window.location = 'checkout.html';
-    // localStorage.removeItem('items')
+    // localStorage.removeItem('items');
 })
-
--- checkout.js
+++ checkout.js
@@ -1,32 +1,38 @@
-const data = localStorage.getItem('items')
+const data = localStorage.getItem('items');
 const items = JSON.parse(data);
 
 console.log(items);
 
+const sameDiv = document.querySelector('.sameDiv');
 
-const div = document.querySelector('.render-phones');
-
-function renderItems(){
-    if(items.length > 0){
-        div.innerHTML = ''
-    for (let i = 0; i < items.length; i++) {
-        div.innerHTML += `
-        <div style="border: 1px solid black;" class="m-5 p-5 rounded">
-        <h1>title: ${items[i].title}</h1> <br/>
-        <img width="200px" src="${items[i].image}" alt="product-image">
-        <h3>price: ${items[i].price}</h3>
-        <button onclick="deleteItem(${i})" class="btn btn-primary m-2 btn-lg">Delete</button>
-        </div>
-        `
-    }
-    }else{
-        div.innerHTML = 'no item found'
+function renderItems() {
+    if (items.length > 0){
+        sameDiv.innerHTML = ''
+        for (let i = 0; i < items.length; i++){
+            // console.log(items[i]);
+            sameDiv.innerHTML += `
+            <div class="card p-1 mb-3 shadow-lg" style="width: 16rem; height: 29rem">
+                <img width ='' height ='200px' src= ${items[i].image} class="card-img-top" alt="Product image">
+            <div class="card-body">
+            <h5 class="card-title titleh5">Title: ${items[i].title}</h5>
+            <p class="card-text">Category: ${items[i].category}</p>
+            <h5 class="card-title">Price: ${items[i].price}</h5>
+            <a onclick="delete2(${i})" class="btn btn-warning">Delete</a>
+            </div>
+            </div>
+            `
+        }        
+    } else {
+        sameDiv.innerHTML = 'No product found in the cart right now!'
     }
 }
-renderItems()
-function deleteItem(index){
+
+renderItems();
+
+
+function delete2(index){
     items.splice(index , 1);
-    renderItems()
-    localStorage.setItem('items' , items);
+    renderItems();
+    localStorage.setItem('items' ,JSON.stringify(items));
 
 }
